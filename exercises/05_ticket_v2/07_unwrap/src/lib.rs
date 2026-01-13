7ea19576e9090d1757f1ca8867bb728f5d3a05e3
jajapuramshivasai <jajapuramshivasai@gmail.com>
2026-01-13 10:55:18 +0530

    progress update

--- exercises/05_ticket_v2/07_unwrap/src/lib.rs
+++ exercises/05_ticket_v2/07_unwrap/src/lib.rs
@@ -2,7 +2,7 @@
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    // todo!()
 }
 
 #[derive(Debug, PartialEq, Clone)]
